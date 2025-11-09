c9a86b0347202a0d96292e0415605d0c4b577e9a
Daniel Gomes <danielcerqueira7747@gmail.com>
2025-11-09 20:49:26 -0300

    fix: problema de display

--- sketch_nov8a.ino
+++ sketch_nov8a.ino
@@ -2,46 +2,35 @@
 // Adicionar EEPROM
 // Configurar Relé
 // Criar App para comunicação com o circuito
-// Criar funções Inicializar e Configurar em .h para importação
-
-// Acessa configurar
-// Configurar quantas doses diárias, se for 2, digite o 1 horário, digite o segundo, quantos ciclos
-
-// Consultar dados na memória para saber se os dados de configuração existem, se sim, iniciar
-// Inicializar, deverá executar o processo de dosagem, verificando minuto por minuto e acompanhado por um botão de sair
-// Deverá mostrar o horário da próxima dosagem
-// Ao fazer os ciclos, ele irá contar , por exemplo, falta 6 ciclos para completar a dosagem
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <ThreeWire.h>
-#include <RtcDS1302.h>
 #include <Keypad.h>
 #include <EEPROM.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
 
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
-#define OLED_ADDR 0x3C
-
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 #define DAT_PIN 11
 #define CLK_PIN 10
 #define RST_PIN 12
 
-#define sizeEEPROM 1000
+ThreeWire myWire(DAT_PIN, CLK_PIN, RST_PIN);
+RtcDS1302<ThreeWire> Rtc(myWire);
 
 const uint8_t X1 = 2;
 const uint8_t X2 = 3;
 const uint8_t X3 = 4;
 const uint8_t X4 = 5;
 const uint8_t X5 = 6;
-const uint8_t X6 = 7; 
+const uint8_t X6 = 7;
 const uint8_t X7 = 8;
 
 const byte row_size = 4;
 const byte col_size = 3;
-
-int state = 0;
 
 const uint8_t row_pin[4] = { X2, X7, X6, X4 };
 const uint8_t col_pin[3] = { X3, X1, X5 };
@@ -54,133 +43,88 @@ const char keys[4][3] = {
 };
 
 Keypad keypad = Keypad(makeKeymap(keys), row_pin, col_pin, row_size, col_size);
-ThreeWire myWire(DAT_PIN, CLK_PIN, RST_PIN);
-RtcDS1302<ThreeWire> Rtc(myWire);
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
 
-char key;
 int selected = 0;
-
-const char* menuOptions[] = {
-  "Inicializar",
-  "Configurar",
-  "Reiniciar"
-};
-const int totalOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
+const int totalOptions = 3;
+int inAction = -1;
 
 void setup() {
 
-  Serial.begin(9600);
-  Rtc.Begin();
-
-  if (!Rtc.GetIsRunning()) {
-
-    Rtc.SetIsRunning(true);
-    Rtc.SetDateTime(RtcDateTime(__DATE__, __TIME__));
-
-  }
-
-  Wire.begin();
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-
-    Serial.println("Falha ao inicializar display OLED.");
-    for (;;);
-
-  }
-
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor((128 - 6 * strlen("Carregando...")) / 2, 20);
   display.println("Carregando...");
   display.display();
-  delay(1500);
 
+  Rtc.Begin();
+
+  if (!Rtc.GetIsRunning() || !Rtc.IsDateTimeValid()) {
+    Rtc.SetIsRunning(true);
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    Rtc.SetDateTime(compiled);
+  }
+
+
+  delay(1000);
 }
 
 void loop() {
 
-  key = keypad.getKey();
+  char key = keypad.getKey();
 
   if (key) {
 
-    if (key == '8') {
+    if (inAction == -1) {
 
-      selected++;
-      if (selected >= totalOptions) selected = 0;
+      if (key == '8') {
 
-    }
+        selected++;
+        if (selected >= totalOptions) selected = 0;
 
-    else if (key == '2') {
+      } else if (key == '2') {
 
-      selected--;
-      if (selected < 0) selected = totalOptions - 1;
+        selected--;
+        if (selected < 0) selected = totalOptions - 1;
 
-    }
+      } else if (key == '#') {
 
-    else if (key == '#') {
-
-      display.clearDisplay();
-      display.setTextSize(1);
-      display.setTextColor(SSD1306_WHITE);
-
-      // Ele não está conseguindo aceitar mais de um display
-      
-      if (menuOptions[selected] == "Inicializar"){
-
-        /*display.setCursor((128 - 6 * strlen("Inicializando...")) / 2, 20);
-        display.print("Inicializando...");
-        display.display();
-        delay(1000);*/
-
-        Serial.println("Inicializar");
-
-      } else if (menuOptions[selected] == "Configurar") {
-
-        /*display.setCursor((128 - 6 * strlen("Modo Configuração")) / 2, 20);
-        display.print("Modo Configuração");
-        display.display();
-        delay(1000);*/
-
-        Serial.println("Configurar");
-
-      } else if (menuOptions[selected] == "Reiniciar") {
-
-        display.setCursor((128 - 6 * strlen("Reiniciando...")) / 2, 20);
-        display.print("Reiniciando...");
-        display.display();
-
-        for (int nL = 0; nL < sizeEEPROM; nL++) {
-
-          EEPROM.update(nL, 0);
-          if (nL % 50 == 0) delay(5);
-
-        }
-
-        delay(300);
-        void (*resetFunc)(void) = 0;
-        resetFunc();
+        inAction = selected;
 
       }
-      
+
+    } else {
+
+      if (key == '*') {
+
+        inAction = -1;
+
+      }
+
     }
 
   }
 
+  if (inAction == -1) {
+
+    drawMenu();
+
+  } else {
+
+    drawAction(inAction);
+
+  }
+
+  delay(100);
+}
+
+void drawMenu() {
+
   RtcDateTime now = Rtc.GetDateTime();
 
-  char buffer[20];
+  printDateTime(now);
 
-  snprintf(buffer, sizeof(buffer), "%02u/%02u/%04u %02u:%02u",
-           now.Day(), now.Month(), now.Year(),
-           now.Hour(), now.Minute());
-
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.print(buffer);
+  const char* options[totalOptions] = {"Inicializar", "Configurar", "Reiniciar"};
 
   for (int i = 0; i < totalOptions; i++) {
 
@@ -198,10 +142,94 @@ void loop() {
 
     }
 
-    display.print(menuOptions[i]);
+    display.print(options[i]);
   }
 
   display.display();
-  delay(100);
+
+}
+
+void drawAction(int action) {
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  switch (action) {
+
+    case 0:
+
+      inicializar();
+      break;
+
+    case 1:
+
+      configurar();
+      break;
+
+    case 2:
+
+      reiniciar();
+      break;
+
+  }
+
+  display.display();
+
+}
+
+void inicializar() {
+
+  // Consultar dados na memória para saber se os dados de configuração existem, se sim, iniciar
+  // Inicializar, deverá executar o processo de dosagem, verificando minuto por minuto e acompanhado por um botão de sair
+  // Deverá mostrar o horário da próxima dosagem
+  // Ao fazer os ciclos, ele irá contar , por exemplo, falta 6 ciclos para completar a dosagem
+
+  display.println("\n* para voltar");
+  display.display();
+  
+}
+
+void configurar() {
+
+  // Acessa configurar
+  // Configurar quantas doses diárias, se for 2, digite o 1 horário, digite o segundo, quantos ciclos
+
+  display.println("\n* para voltar");
+  display.display();
+
+}
+
+void reiniciar() {
+
+  display.setCursor((128 - 6 * strlen("Reiniciando...")) / 2, 20);
+  display.print("Reiniciando...");
+  display.display();
+
+  for (int i = 0; i < EEPROM.length(); i++) {
+
+    EEPROM.write(i, 0);
+
+  }
+
+  asm volatile ("jmp 0");
+
+}
+
+void printDateTime(const RtcDateTime& dt) {
+
+  char buffer[20];
+
+  snprintf_P(buffer, 
+    sizeof(buffer),
+    PSTR("%02u/%02u/%04u %02u:%02u"),
+    dt.Day(), dt.Month(), dt.Year(),
+    dt.Hour(), dt.Minute());
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(buffer);
+
 }
 
